7d59839d04548d371555bae85031a10c774f947f
rhanilham <rhanilhamfadlillatulr@gmail.com>
2025-07-15 09:14:03 +0700

    update fe dashboard, login

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'My App')</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/logos/logo-pln1.png') }}" />
+    <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}" />
+</head>
+
+<body>
+    <!-- Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <!-- Sidebar Start -->
+        <aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div>
+                <div class="brand-logo d-flex align-items-center justify-content-between">
+                    <a href="./index.html" class="text-nowrap logo-img">
+                        <img src="{{ asset('images/logos/logo-pln2.png') }}" width="180" alt="" />
+                    </a>
+                    <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                        <i class="ti ti-x fs-8"></i>
+                    </div>
+                </div>
+                <!-- Sidebar navigation -->
+                @include('partials.navbar')
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll -->
+        </aside>
+        <!-- Sidebar End -->
+        <!-- Main wrapper -->
+        <div class="body-wrapper">
+            <!-- Header Start -->
+            @include('partials.header')
+            <!-- Header End -->
+            <div class="container-fluid">
+                <!-- Footer Start -->
+                @yield('content')
+                @include('partials.footer')
+                <!-- Footer End -->
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/sidebarmenu.js') }}"></script>
+    <script src="{{ asset('js/app.min.js') }}"></script>
+    <script src="{{ asset('libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('libs/simplebar/dist/simplebar.js') }}"></script>
+    <script src="{{ asset('js/dashboard.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,56 @@
+<nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+    <ul id="sidebarnav">
+        <li class="nav-small-cap">
+            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+            <span class="hide-menu">Beranda</span>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="./index.html" aria-expanded="false">
+                <span>
+                    <i class="ti ti-layout-dashboard"></i>
+                </span>
+                <span class="hide-menu">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-small-cap">
+            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+            <span class="hide-menu">PROPOSAL</span>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="./ui-buttons.html" aria-expanded="false">
+                <span>
+                    <i class="ti ti-article"></i>
+                </span>
+                <span class="hide-menu">Pengajuan</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
+                <span>
+                    <i class="	ti ti-eye"></i>
+                </span>
+                <span class="hide-menu">Monitoring</span>
+            </a>
+        </li>
+        <li class="nav-small-cap">
+            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+            <span class="hide-menu">Manajemen Data</span>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
+                <span>
+                    <i class="ti ti-user"></i>
+                </span>
+                <span class="hide-menu">Pengguna</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="./authentication-register.html" aria-expanded="false">
+                <span>
+                    <i class="ti ti-clipboard"></i>
+                </span>
+                <span class="hide-menu">Tipologi</span>
+            </a>
+        </li>
+    </ul>
+</nav>
